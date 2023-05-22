1a35d9ef2a12a162e7b068652e9a2197d051368f
Eugene Luhovyi <logovoj20@gmail.com>
2023-05-22 10:43:24 +0300

    добавил изменения в классы MonoBankCurrencyService и MonoBankCurrencyItem

--- src/main/java/currency/mono/MonoBankCurrencyService.java
+++ src/main/java/currency/mono/MonoBankCurrencyService.java
@@ -1,4 +1,59 @@
 package currency.mono;
 
-public class MonoBankCurrencyService {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import currency.Currency;
+import currency.CurrencyService;
+import currency.mono.dto.MonoBankCurrencyItem;
+import org.jsoup.Jsoup;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class MonoBankCurrencyService implements CurrencyService {
+    private final static String url = "https://api.monobank.ua/bank/currency";
+    private final static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    List<MonoBankCurrencyItem> list;
+
+    private void getExchangeRates(){
+        String rates;
+        try {
+            rates = Jsoup.connect(url)
+                    .ignoreContentType(true)
+                    .get()
+                    .text();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Type type = TypeToken.getParameterized(List.class, MonoBankCurrencyItem.class).getType();
+        list = gson.fromJson(rates, type);
+    }
+
+
+    @Override
+    public double getRateBuy(Currency currency) {
+        getExchangeRates();
+        return list.stream()
+                .filter(it -> it.getCurrency() == currency)
+                .filter(it -> it.getBaseCurrency() == Currency.UAH)
+                .map(MonoBankCurrencyItem::getRateBuy)
+                .findFirst().orElse(-1d);
+
+    }
+
+    @Override
+    public double getRateSell(Currency currency) {
+        getExchangeRates();
+        return list.stream()
+                .filter(it -> it.getCurrency() == currency)
+                .filter(it -> it.getBaseCurrency() == Currency.UAH)
+                .map(MonoBankCurrencyItem::getRateSell)
+                .findFirst().orElse(-1d);
+
+    }
+    
 }
+
--- src/main/java/currency/mono/dto/MonoBankCurrencyItem.java
+++ src/main/java/currency/mono/dto/MonoBankCurrencyItem.java
@@ -1,4 +1,36 @@
 package currency.mono.dto;
 
+import currency.Currency;
+import lombok.Data;
+
+@Data
 public class MonoBankCurrencyItem {
+    private int currencyCodeA;
+    private int currencyCodeB;
+    private long date;
+    private double rateBuy;
+    private double rateCross;
+    private double rateSell;
+    private Currency baseCurrency;
+    private Currency currency;
+
+
+
+    public Currency getCurrency(){
+        if(currencyCodeA == 840){
+            currency = Currency.USD;
+        }
+        if(currencyCodeA == 978){
+            currency = Currency.EUR;
+        }
+        return currency;
+    }
+
+    public Currency getBaseCurrency(){
+
+        if(currencyCodeB == 980){
+            baseCurrency = Currency.UAH;
+        }
+        return baseCurrency;
+    }
 }
