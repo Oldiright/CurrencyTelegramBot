ddf1e22eccf7fa617569fd0c8a0ad68fdda0a606
olena0112 <124000255+olena0112@users.noreply.github.com>
2023-05-22 17:45:20 +0300

    PrivatBank

--- src/main/java/currency/privat/dto/PrivatBankCurrencyItem.java
+++ src/main/java/currency/privat/dto/PrivatBankCurrencyItem.java
@@ -1,4 +1,20 @@
 package currency.privat.dto;
-
+import currency.Currency;
+import lombok.Data;
+@Data
 public class PrivatBankCurrencyItem {
+    private Currency ccy;
+    private Currency base_ccy;
+    private float buy;
+    private float sale;
+    @Override
+    public String toString() {
+        return "CurrencyItem{" +
+                "ccy=" + ccy +
+                ", base_ccy=" + base_ccy +
+                ", buy=" + buy +
+                ", sale=" + sale +
+                '}';
+    }
+
 }
