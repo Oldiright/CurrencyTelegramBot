b7badaca94fca73f28683923f7dbda4b96ed0b70
Dmytro Oliinyk <oliynuk_dima@ukr.net>
2023-05-23 20:53:49 +0300

    without token and name

--- src/main/java/telegram/BotConstants.java
+++ src/main/java/telegram/BotConstants.java
@@ -1,6 +1,6 @@
 package telegram;
 
 public class BotConstants {
-    public static  final String BOT_TOKEN = "5963517481:AAFanyMODjOHCbGgW0d0WGjXOx6zvB2_Q6A";
-    public static  final String BOT_NAME = "UltraBanderogusBot";
+    public static  final String BOT_TOKEN = "";
+    public static  final String BOT_NAME = "";
 }
