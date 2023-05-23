fcebbffe062d2be467cc32ca93a8a4caa6e0ed7a
odin0960 <121524275+odin0960@users.noreply.github.com>
2023-05-23 14:52:55 +0300

    Проста версія BotConstants (заглушка)

---
+++ src/main/java/telegram/BotConstants.java
@@ -0,0 +1,11 @@
+package telegram;
+
+import lombok.Data;
+
+@Data
+class BotConstants {
+
+    public static final String BOT_NAME = "GoIT_project_group_2_CurrencyBot";
+    public static final String BOT_TOKEN = "6085336025:AAEsoZGop_-X4ey7EGXxye0xQQAinH83p1U";
+
+}
